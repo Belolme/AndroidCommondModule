d83000ca460ec4289b13a90c860ef83cad9ad0d4
billin <billindeng@outlook.com>
2021-01-13 17:55:37 +0800

    add and use view binding

--- playground/src/main/java/com/billin/com/playground/MainActivity.kt
+++ playground/src/main/java/com/billin/com/playground/MainActivity.kt
@@ -2,11 +2,16 @@ package com.billin.com.playground
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.billin.com.playground.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.helloText.text = "Hello World!"
     }
 }
